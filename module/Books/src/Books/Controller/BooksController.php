deac2290cf6a727c81fa5261a0a03bc64b2fb42f
Luiz Henrique Santos <lhsantosazs@gmail.com>
2014-06-23 21:22:39 -0300

    retirada de codigos desnecessários

--- module/Books/src/Books/Controller/BooksController.php
+++ module/Books/src/Books/Controller/BooksController.php
@@ -56,9 +56,9 @@ class BooksController extends AbstractActionController {
     public function getByTitleAction() {
         //recebe o parâmetro title
         $title = $this->getEvent()->getRouteMatch()->getParam('title');
-        
+
         //Substitui o caracter '+' por ' ' para fazer a pesquisa
-        $title = str_replace ('+' , ' ' , $title);
+        $title = str_replace('+', ' ', $title);
 
         //busca do book com o titulo
         $books = $this->getEntityManager()->getRepository('Books\Entity\Books')->findBy(array('title' => $title));
@@ -73,10 +73,10 @@ class BooksController extends AbstractActionController {
     public function getByAuthorAction() {
         //recebe o parâmetro author
         $author = $this->getEvent()->getRouteMatch()->getParam('author');
-        
+
         //Substitui o caracter '+' por ' ' para fazer a pesquisa
-        $author = str_replace ('+' , ' ' , $author);
-        
+        $author = str_replace('+', ' ', $author);
+
         //busca do book pelo autor
         $books = $this->getEntityManager()->getRepository('Books\Entity\Books')->findBy(array('author' => $author));
 
@@ -95,8 +95,8 @@ class BooksController extends AbstractActionController {
         $author = $this->getEvent()->getRouteMatch()->getParam('author');
 
         //Substitui o caracter '+' por ' ' para fazer a pesquisa
-        $title = str_replace ('+' , ' ' , $title);
-        $author = str_replace ('+' , ' ' , $author);
+        $title = str_replace('+', ' ', $title);
+        $author = str_replace('+', ' ', $author);
 
         //busca do book com o id
         $books = $this->getEntityManager()->getRepository('Books\Entity\Books')->findBy(array('title' => $title, 'author' => $author));
@@ -105,74 +105,6 @@ class BooksController extends AbstractActionController {
         return new JsonModel(array(
             'books' => $books,
         ));
-    }
-
-    /* Não necessário */
-
-    public function editAction() {
-        $id = (int) $this->getEvent()->getRouteMatch()->getParam('id');
-        if (!$id) {
-            return $this->redirect()->toRoute('books', array('action' => 'add'));
-        }
-        $books = $this->getEntityManager()->find('Books\Entity\Books', $id);
-
-        $form = new BooksForm();
-        $form->setBindOnValidate(false);
-        $form->bind($books);
-        $form->get('submit')->setAttribute('label', 'Edit');
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            //$form->setData($request->post());
-            $form->setData($request->getPost());
-            if ($form->isValid()) {
-                $form->bindValues();
-                $this->getEntityManager()->flush();
-
-// Redirect to list of books
-                return $this->redirect()->toRoute('books');
-            }
-        }
-
-        return array(
-            'id' => $id,
-            'form' => $form,
-        );
-    }
-
-    /* Não necessário */
-
-    public function deleteAction() {
-        $id = (int) $this->getEvent()->getRouteMatch()->getParam('id');
-        if (!$id) {
-            return $this->redirect()->toRoute('books');
-        }
-
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            //$del = $request->post()->get('del', 'No');
-            $del = $request->getPost()->get('del', 'No');
-
-            if ($del == 'Yes') {
-                //$id = (int) $request->post()->get('id');
-                $id = (int) $request->getPost()->get('id');
-                $books = $this->getEntityManager()->find('Books\Entity\Books', $id);
-                if ($books) {
-                    $this->getEntityManager()->remove($books);
-                    $this->getEntityManager()->flush();
-                }
-            }
-
-// Redirect to list of books
-            return $this->redirect()->toRoute('default', array(
-                        'controller' => 'books',
-                        'action' => 'index',
-            ));
-        }
-
-        return array(
-            'id' => $id,
-            'books' => $this->getEntityManager()->find('Books\Entity\Books', $id)->getArrayCopy()
-        );
     }
 
 }
